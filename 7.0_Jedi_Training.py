d0c275236c57ac29399b07fc4d238a22d3037392
huynhlin <huynhlin@urbandaleschools.com>
2022-11-11 09:15:28 -0600

    finished jedi training

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -1,7 +1,28 @@
-#Sign your name:________________
+# Sign your name: Lindy Huynh
 
 '''
 Recreate, exactly the Test Picture from the website. The arcade colors used in this picture in no particular order are:
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
 '''
+import arcade
+arcade.open_window(500, 400, "chapter 7 test image")
+arcade.set_background_color(arcade.color.ALMOND)
+arcade.start_render()
+x_offset = 0
+y_offset = 0
+for x in range(25): # GRID LINES
+    arcade.draw_line(20 + x_offset, 0, 20 + x_offset, 500, arcade.color.BLACK, 1)
+    x_offset += 20
+    arcade.draw_line(0, 20 + y_offset, 500, 20 + y_offset, arcade.color.BLACK, 1)
+    y_offset += 20
+arcade.draw_point(460, 10, arcade.color.RED, 5) # red dot in bot right
+arcade.draw_line(80, 20, 120, 60, arcade.color.BLUE, 1) # blue line in bot left
+arcade.draw_rectangle_filled(50, 370, 60, 20, arcade.color.PHLOX) # rectangle top left
+arcade.draw_circle_filled(250, 200, 40, arcade.color.WISTERIA) # center circle
+arcade.draw_rectangle_filled(200, 260, 40, 20, arcade.color.BLUSH, 135) # tilted rectangle mid
+arcade.draw_ellipse_filled(100, 100, 120, 40, arcade.color.AMBER) # amber ellipse bot left
+arcade.draw_text("I love you. I know.", 20, 160, arcade.color.BRICK_RED, 20) # text next to circle
+arcade.draw_arc_filled(400, 320, 120, 120, arcade.color.YELLOW, 0, 300, 30) # pac man - angles are very hard holyy that took a while
+arcade.finish_render()
+arcade.run()
--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -10,3 +10,23 @@ We will have a competition to see who can make this flag in the least lines of c
 The record is 16! You will have to use some loops to achieve this.
 CHALLENGE: Can you make the entire flag parametrically? This means if I change the hoist to 520px the flag will resize accordingly.
 '''
+import arcade
+arcade.open_window(500,500, "Stars and Stripes")
+arcade.start_render()
+
+arcade.draw_rectangle_filled(250,250,400,260,arcade.color.RED)
+# blue rectangle under stars
+ex = 15
+why = 100
+counter = 0
+for x in range(50):
+    arcade.draw_text(" * ", 39 + ex, 250 + why, arcade.color.WHITE, 20)
+    ex += 15
+    counter += 1
+    if counter == 10:
+        counter = 0
+        why -= 15
+        ex = 15
+# white stripes
+arcade.finish_render()
+arcade.run()
